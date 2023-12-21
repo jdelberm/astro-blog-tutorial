4579ddddecf7c98a6417dd3f8aa5ac3e45b90d13
JDB <juandb95@hotmail.com>
2023-12-21 21:20:50 +0100

    feat: animation for hamburger when it's opened

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,7 @@
+
 document.querySelector(".hamburger").addEventListener("click", () => {
+  document.querySelector(".upper").classList.toggle("tilted-down");
+  document.querySelector(".middle").classList.toggle("hidden");
+  document.querySelector(".lower").classList.toggle("tilted-up");
   document.querySelector(".navbar").classList.toggle("expanded");
 });
