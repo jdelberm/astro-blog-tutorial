d375ec2b152baa73daf062728e6b7088e865297d
jdelgado <jdelgado.berm@gmail.com>
2023-12-23 11:37:07 +0100

    feat: swapped greeting randomization to be automatic

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -4,8 +4,11 @@ export default function Greeting({ messages }) {
 
     const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
 
-    const [greeting, setGreeting] = useState(messages[0]);
+    //const [greeting, setGreeting] = useState(messages[0]);
+    const [greeting, setGreeting] = useState(randomMessage);
 
+    /*
+    Button triggered greeting randomization
     return (
         <div>
             <h3>{greeting}! Thank you for visiting!</h3>
@@ -13,5 +16,12 @@ export default function Greeting({ messages }) {
                 New Greeting
             </button>
         </div >
+    )
+     */
+    //Greeting randomization by default
+    return (
+        <div>
+            <h3>{greeting}! Thank you for visiting!</h3>
+        </div>
     )
 }
