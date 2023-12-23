117ba31bd04c1efabd67ae09f1e80a93a5342095
jdelgado <jdelgado.berm@gmail.com>
2023-12-23 12:44:00 +0100

    refactor: simplified cross format by grouping animations

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,7 +1,5 @@
 
 document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".upper").classList.toggle("tilted-down");
-  document.querySelector(".middle").classList.toggle("hidden");
-  document.querySelector(".lower").classList.toggle("tilted-up");
+  document.querySelector(".hamburger").classList.toggle("cross");
   document.querySelector(".navbar").classList.toggle("expanded");
 });
